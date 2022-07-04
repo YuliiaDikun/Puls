efacaafbcdd1668e34bb912c4cfd4a42d232a4b6
YuliiaDikun <dikun.yulia@gmail.com>
2022-07-04 16:52:31 +0300

    validate index.html

--- src/js/script.js
+++ src/js/script.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
             $(this).on('click', function(e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                $('.catalog-item__block').eq(i).toggleClass('catalog-item__block_active');
             })
         })
     };
@@ -109,7 +109,7 @@ $(document).ready(function(){
         }
     });
 
-    $("a[href^='#']").click(function() {
+    $("a[href^='#up']").click(function() {
         const _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
